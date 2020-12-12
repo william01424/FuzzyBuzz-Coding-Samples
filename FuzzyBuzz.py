bbb710f0c90107f270452864dd1f973a27baa54f
Will Templeton <75176144+william01424@users.noreply.github.com>
2020-12-12 21:01:56 +0000

    FuzzyBuzzy Challenge
    Multiple attempts (similar but slightly different)

---
+++ FuzzyBuzz.py
@@ -0,0 +1,33 @@
+# for i in range(1,31):
+#     if i%15 == 0:
+#         print(i,'.', '\tFuzzy Buzz', sep='')
+#     elif i%3 == 0:
+#         print(i,'.', '\tFuzzy', sep='')
+#     elif i%5 == 0:
+#          print(i,'.', '\tBuzz', sep='')
+
+for i in range(1,31):
+    if i % 3 == 0 and i%15 != 0:
+        print(i, '.', '\tFuzzy', sep='')
+    elif i % 5 == 0 and i%15 != 0:
+        print(i, '.', '\tBuzz', sep='')
+    elif i%3 == 0 and i%5 == 0:
+        print(i, '.', '\tFuzzy Buzz', sep='')
+
+for i in range(1,31):
+    if i % 3 == 0 and i%15 != 0:
+        print(i, '.', '\tFuzzy', sep='')
+    elif i % 5 == 0 and i%15 != 0:
+        print(i, '.', '\tBuzz', sep='')
+    elif i%3 == 0 and i%5 == 0:
+        print(i, '.', '\tFuzzy Buzz', sep='')
+
+# list = list(range(1, 31))
+#
+# for i in list:
+#     if i%15 == 0:
+#         print(i,'.', '\tFozzie Bear', sep='')
+#     elif i%5 == 0:
+#         print(i, '.', '\tBear', sep='')
+#     elif i%3 == 0:
+#         print(i,'.', '\tFozzie', sep='')
